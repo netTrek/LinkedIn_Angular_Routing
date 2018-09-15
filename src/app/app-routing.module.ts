0402637e820c070e8d924d3503529596e6fdd35b
producer <producer@highsierra1.content.linkedin.biz>
2018-09-15 14:19:17 +0200

    DE_3193_02_02_1_Projektsetup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: 'user', component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

cbe3846512f203e5ebb4e5b3a1d3a571f1b80eea
producer <producer@highsierra1.content.linkedin.biz>
2018-09-15 14:55:47 +0200

    DE_3193_03_02_useHash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
